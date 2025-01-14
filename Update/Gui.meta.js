a04734b3e0ce6b0f9ad576f2b091e3bbf6b64b59
randomstuff69 <132950785+randomstuff69@users.noreply.github.com>
2025-01-13 18:48:41 -0700

    Update Gui.meta.js

--- Update/Gui.meta.js
+++ Update/Gui.meta.js
@@ -2,9 +2,10 @@
 // @name         Blooket Cheats Plus
 // @namespace    https://github.com/randomstuff69/blooketcheatsplus
 // @version      15.4.1
+// @supportURL   https://discord.gg/7FQCXbchrc
 // @description  Blooket Cheats Plus
 // @updateURL    https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/main/Update/Gui.meta.js
-// @downloadURL  https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/main/GUI/Gui.js
+// @downloadURL  https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/main/GUI/Gui.user.js
 // @author       DannyDan0167 and Cool Duck
 // @match        *://*.blooket.com/*
 // @icon         https://i.ibb.co/sWqBm0K/1024.png
