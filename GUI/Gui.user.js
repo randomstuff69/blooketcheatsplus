d371a25bc64c23fc18b2dfb2223eb9c764e2c1ea
randomstuff69 <132950785+randomstuff69@users.noreply.github.com>
2025-02-15 13:16:40 -0700

    Update Gui.user.js

--- GUI/Gui.user.js
+++ GUI/Gui.user.js
@@ -3,11 +3,11 @@
 // @namespace    https://github.com/randomstuff69/blooketcheatsplus
 // @version      15
 // @description  Blooket Cheats Plus
+// @updateURL    https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/main/Update/Gui.meta.js
+// @downloadURL  https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/main/GUI/Gui.user.js
 // @author       DannyDan0167 and Cool Duck
 // @match        *://*.blooket.com/*
 // @icon         https://i.ibb.co/sWqBm0K/1024.png
 // @grant        none
-// @require     https://unpkg.com/idb-keyval@6.0.3/dist/umd.js
+// @require     https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/refs/heads/main/GUI/Gui.js
 // ==/UserScript==
-
-fetch('https://cdn.jsdelivr.net/gh/randomstuff69/blooketcheatsplus/GUI/Gui.min.js').then(r=>r.text()).then(eval).catch(e=>console.error('Error:',e));
